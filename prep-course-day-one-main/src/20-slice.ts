96fcb60ae151817a78e036790c325c4b56ddfc09
eamon9 <81514128+eamon9@users.noreply.github.com>
2021-07-30 21:56:25 +0300

    Update 20-slice.ts

--- prep-course-day-one-main/src/20-slice.ts
+++ prep-course-day-one-main/src/20-slice.ts
@@ -3,6 +3,10 @@ export {};
 /**
  * Write function first5 which returns first five elements from the array
  */
+function first5(arr) {
+  var num= arr.slice(0, 5);
+  return num;
+}
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8]; // You are NOT allowed to edit this line
 const first5Numbers = first5(numbers); // You are NOT allowed to edit this line
