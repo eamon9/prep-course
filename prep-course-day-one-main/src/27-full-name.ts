38ab75698434b5d4071d9e75d5341c22f327195f
eamon9 <81514128+eamon9@users.noreply.github.com>
2021-07-31 01:37:09 +0300

    Update 27-full-name.ts

--- prep-course-day-one-main/src/27-full-name.ts
+++ prep-course-day-one-main/src/27-full-name.ts
@@ -3,5 +3,8 @@ export {};
 /**
  * Create a function called fullName, which concatenates two strings together
  */
+function fullName(str1, str2) {
+  return str1+" "+str2
+}
 
 console.log(fullName("John", "Doe")); //Expected output: 'John Doe'
