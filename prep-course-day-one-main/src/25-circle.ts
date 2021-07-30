8d9508b19b631da7de43ad7e1007ae65e0c4a113
eamon9 <81514128+eamon9@users.noreply.github.com>
2021-07-31 00:23:50 +0300

    Update 25-circle.ts

--- prep-course-day-one-main/src/25-circle.ts
+++ prep-course-day-one-main/src/25-circle.ts
@@ -2,10 +2,13 @@ export {};
 
 function Circle(radius) {
   this.radius = radius;
-  this.area = function() {};
-  this.perimeter = function() {};
+  this.area = function() {
+    return Math.PI*this.radius*this.radius;
+  };
+  this.perimeter = function() {
+    return Math.PI*this.radius*2};
 }
 
 const c = new Circle(3);
-console.log("Area =", c.area()); // Expected output: Area = 28.27
-console.log("Perimeter =", c.perimeter()); // Expected output: Perimeter = 18.85
+console.log("Area =", c.area().toFixed(2)); // Expected output: Area = 28.27
+console.log("Perimeter =", c.perimeter().toFixed(2)); // Expected output: Perimeter = 18.85
