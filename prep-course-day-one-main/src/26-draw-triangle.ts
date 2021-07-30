6ebc10d65b598df9486356968da8e947f43eb87e
eamon9 <81514128+eamon9@users.noreply.github.com>
2021-07-31 01:33:54 +0300

    Update 26-draw-triangle.ts

--- prep-course-day-one-main/src/26-draw-triangle.ts
+++ prep-course-day-one-main/src/26-draw-triangle.ts
@@ -1,7 +1,17 @@
 export {};
 
-function draw() {}
-
+function draw(multiplyBy) {
+    let stars = "";
+    for(let i = 1; i <= multiplyBy; i++ ) {
+        for(let j = 1; j <= multiplyBy; j++) {
+            if(i >= j ) {
+                stars = stars.concat("*");
+            }
+        }
+        stars = stars.concat("\n");
+    }
+    console.log(stars);
+}
 draw(3);
 /* Expected output:
 
