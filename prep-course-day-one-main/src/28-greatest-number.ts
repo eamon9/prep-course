a81a04ac526aab716724c6e83cd2840e6e912f31
eamon9 <81514128+eamon9@users.noreply.github.com>
2021-07-31 09:33:44 +0300

    Update 28-greatest-number.ts

--- prep-course-day-one-main/src/28-greatest-number.ts
+++ prep-course-day-one-main/src/28-greatest-number.ts
@@ -1,6 +1,16 @@
 export {};
 
-function greatest(x, y) {}
+function greatest(x, y) {
+  if (x>y) {
+    return x;
+  }
+  else if (x<y) {
+    return y;
+  }
+  else {
+    return "Numbers are equally!";
+  }
+}
 
 console.log(greatest(1, 2)); // Expected output: 2
 console.log(greatest(5, 2)); // Expected output: 5
