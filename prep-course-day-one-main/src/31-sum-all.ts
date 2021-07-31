713e44ec9053805b7aa4404a023097ec04e18955
eamon9 <81514128+eamon9@users.noreply.github.com>
2021-07-31 16:09:54 +0300

    Update 31-sum-all.ts

--- prep-course-day-one-main/src/31-sum-all.ts
+++ prep-course-day-one-main/src/31-sum-all.ts
@@ -6,6 +6,17 @@ export {};
  *  - 1, 4 will return 1 + 2 + 3 + 4 which is 10
  */
 
-const sumAll = function() {};
+const sumAll = function(a, b) {
+  var arr= [];
+  while(a <= b) {
+    arr.push(a++);
+  }
+  var sum = 0;
+  for(let i=0; i<arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum
+  
+};
 
 console.log(sumAll(1, 4)); // Expected output: 10
