761ce7d8463294cd74acad4e933c6bdcc32025a1
eamon9 <81514128+eamon9@users.noreply.github.com>
2021-07-31 23:51:05 +0300

    Update 33-temperature-conversion.ts

--- prep-course-day-one-main/src/33-temperature-conversion.ts
+++ prep-course-day-one-main/src/33-temperature-conversion.ts
@@ -8,8 +8,14 @@ export {};
  * Temperature should be rounded to one decimal place: i.e., fahrenheitToCelsius(100) should return 37.8 and not 37.77777777777778.
  */
 
-const fahrenheitToCelsius = ?
-const celsiusToFahrenheit = ?
+const fahrenheitToCelsius = temperature => {
+  let FTC= ((temperature-32)*(5/9)).toFixed(1);
+  return FTC;
+}
+const celsiusToFahrenheit = temperature => {
+  let CTF= ((temperature*1.8)+32).toFixed(1);
+  return CTF;
+}
 
 console.log(fahrenheitToCelsius(32)); // Expected result: 0
 console.log(celsiusToFahrenheit(0)); // Expected result: 32
