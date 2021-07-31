f5463a1f461f23df68706f24f039504eb31c866e
eamon9 <81514128+eamon9@users.noreply.github.com>
2021-07-31 11:18:08 +0300

    Update 29-is-blank.ts

--- prep-course-day-one-main/src/29-is-blank.ts
+++ prep-course-day-one-main/src/29-is-blank.ts
@@ -4,6 +4,15 @@ export {};
  * Create a function called isBlank, which checks if passed string is blank or not
  */
 
+function isBlank(adj) {
+  if (adj === "" || adj === " " || adj === null) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
 console.log(isBlank(null)); // Expected output: true
 console.log(isBlank("")); // Expected output: true
 console.log(isBlank(" ")); // Expected output: true
