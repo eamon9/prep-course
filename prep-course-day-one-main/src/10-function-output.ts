59231d02323768792f3a443fb4dc19a5e3d69438
eamon9 <81514128+eamon9@users.noreply.github.com>
2021-08-02 01:30:43 +0300

    Update 10-function-output.ts

--- prep-course-day-one-main/src/10-function-output.ts
+++ prep-course-day-one-main/src/10-function-output.ts
@@ -1,7 +1,7 @@
 export {};
 
 function s(w1, w2) {
-  return w1+" "+w2;
+  return w1.concat(' ').concat(w2);;
 }
 
 const result = s("hello", "world"); // concatenate two strings - 'hello', 'world', using the function above
