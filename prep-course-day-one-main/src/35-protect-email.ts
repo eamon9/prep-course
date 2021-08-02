40f5464bad5a738947468f731bd54eb56316df1a
eamon9 <81514128+eamon9@users.noreply.github.com>
2021-08-02 16:59:47 +0300

    Update 35-protect-email.ts

--- prep-course-day-one-main/src/35-protect-email.ts
+++ prep-course-day-one-main/src/35-protect-email.ts
@@ -3,6 +3,19 @@ export {};
 /**
  * Create a function protectEmail which hides some symbols of the email
  */
+function protectEmail(email) {
+  let avg;
+  let split;
+  let p1;
+  let p2;
+  split= email.split("@"); // 'secret123 ' and 'codelex.io'
+  p1= split[0]; // 'secret123 '
+  avg= p1.length/2; // 9/2= 4.5
+  p1= p1.slice(0, (p1.length- avg)); // secr
+  p2= split[1]; // codelex.io
+  return p1+ "...@"+ p2;
+}
+
 
 console.log(protectEmail("secret123@codelex.io")); // Expected result: sec...@codelex.io
 console.log(protectEmail("example@example.com")); // Expected result: exa...@example.com
