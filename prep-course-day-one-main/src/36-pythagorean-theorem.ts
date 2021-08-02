84b2845b7045addac5ef83a8ffc4261ec395e7a4
eamon9 <81514128+eamon9@users.noreply.github.com>
2021-08-02 23:59:53 +0300

    Update 36-pythagorean-theorem.ts

--- prep-course-day-one-main/src/36-pythagorean-theorem.ts
+++ prep-course-day-one-main/src/36-pythagorean-theorem.ts
@@ -6,7 +6,9 @@ export {};
  * https://en.wikipedia.org/wiki/Pythagorean_theorem
  */
 
-const pythagoreanTheorem = () => {};
+const pythagoreanTheorem = (side1: number, side2: number): number => {
+  return Math.sqrt(Math.pow(side1, 2) + Math.pow(side2, 2));
+};
 
 console.log(pythagoreanTheorem(2, 4)); // Expected result: 4.47213595499958
 console.log(pythagoreanTheorem(3, 4)); // Expected result: 5
