32d47cc3740289fdb44dbf9db9a2c74c651360e9
eamon9 <81514128+eamon9@users.noreply.github.com>
2021-08-02 13:06:31 +0300

    Update 13-truncate-string.ts

--- prep-course-day-one-main/src/13-truncate-string.ts
+++ prep-course-day-one-main/src/13-truncate-string.ts
@@ -1,7 +1,7 @@
 export {};
 
-function truncateString(name, firstCh) {
-  return name.slice(0, firstCh);
+function truncateString(name, end) {
+  return name.slice(0, end);
 }
 
 console.log(truncateString("CODELEX", 4)); // Expected output: CODE
