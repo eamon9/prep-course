643191b5dcd325ed558505b4b38abcf9c3a87f5a
eamon9 <81514128+eamon9@users.noreply.github.com>
2021-08-02 13:35:27 +0300

    Update 34-multiply-map.ts
    Nomainīju garo funkciju uz īsāku/ saprotamāku!

--- prep-course-day-one-main/src/34-multiply-map.ts
+++ prep-course-day-one-main/src/34-multiply-map.ts
@@ -4,10 +4,11 @@ export {};
  * Implement map function which works similarly as https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
  */
 
-const map = () => {};
+const map= (arr) => {
+  let result = arr.map(nr => nr * 2);
+  return result;
+};
 
 const numbers = [1, 2, 3];
-const doubled = map(numbers, function(number) {
-  return number * 2;
-});
+const doubled = map(numbers);
 console.log(doubled); // Expected result: [2, 4, 6]
