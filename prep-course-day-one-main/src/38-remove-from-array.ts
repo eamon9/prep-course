f7ee3ad3bb968e6ae0287ec0d91b7ed0d6bc3c01
eamon9 <81514128+eamon9@users.noreply.github.com>
2021-08-03 02:12:30 +0300

    Update 38-remove-from-array.ts

--- prep-course-day-one-main/src/38-remove-from-array.ts
+++ prep-course-day-one-main/src/38-remove-from-array.ts
@@ -1,6 +1,13 @@
 export {};
 
-const removeFromArray = function() {};
+const removeFromArray = function(arr, ...toRemove) {
+  for (let i = 0; i < toRemove.length; i++) {
+      let index = arr.indexOf(toRemove[i]);
+      if (index === -1) continue;
+      arr.splice(index, 1);
+  }
+  return arr;
+}
 
 console.log(removeFromArray([1, 2, 3, 4], 3)); // Expected output: [1, 2, 4]
 console.log(removeFromArray([1, 2, 3, 4], 7)); // Expected output: [1, 2, 3, 4]
