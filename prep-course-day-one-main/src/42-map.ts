61fc4efc1d1f70f937f049fa77b919f9629f9d13
eamon9 <81514128+eamon9@users.noreply.github.com>
2021-08-03 23:00:35 +0300

    Update 42-map.ts
    Not Working function charAt(0)

--- prep-course-day-one-main/src/42-map.ts
+++ prep-course-day-one-main/src/42-map.ts
@@ -8,9 +8,12 @@ function tidyUpString(str) {
 }
 
 // You are allowed to edit this function
-function capitalise(str) {}
+function capitalise(str) {
+  return str.charAt(0).toUpperCase()+ str.slice(1);
+}
+
 
 const mentors = ["/Daniel ", "irina ", " Gordon", "ashleigh "];
-let mentorsTidy; // You are allowed to edit this line
+let mentorsTidy= capitalise(mentors); // You are allowed to edit this line
 
 console.log(mentorsTidy); // Expected output: ["Daniel", "Irina", "Gordon", "Ashleigh"]
