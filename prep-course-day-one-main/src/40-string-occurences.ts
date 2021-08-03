1adc34d5a9807990f023e0305e050757c40e0e0b
eamon9 <81514128+eamon9@users.noreply.github.com>
2021-08-03 10:24:49 +0300

    Update 40-string-occurences.ts

--- prep-course-day-one-main/src/40-string-occurences.ts
+++ prep-course-day-one-main/src/40-string-occurences.ts
@@ -1,6 +1,8 @@
 export {};
 
-const count = () => {};
+function count(str: string, word: string): number {
+  return str.toLowerCase().split(word.toLowerCase()).length-1;
+}
 
 console.log(count("The quick brown fox jumps over the lazy dog", "the")); // Expected output: 2
-console.log(count("The quick brown fox jumps over the lazy dog", "fox")); // Expected output: 1
+console.log(count("The quick brown fox jumps over the lazy dog", "ox")); // Expected output: 1
