f90e94335cd72d7ab9538fce0b0e9c0e3aadb7c3
eamon9 <81514128+eamon9@users.noreply.github.com>
2021-08-03 10:07:20 +0300

    Update 39-repeat-string.ts

--- prep-course-day-one-main/src/39-repeat-string.ts
+++ prep-course-day-one-main/src/39-repeat-string.ts
@@ -1,6 +1,11 @@
 export {};
 
-const repeatString = () => {};
+const repeatString = (text, multiplier) => {
+  for (let i = 0; i < multiplier-1; i++) {
+    text+= text[i];
+  }
+  return `'`+text+`'`;
+};
 
 console.log(repeatString("a", 4)); // Expected output: 'aaaa'
 console.log(repeatString("b", 5)); // Expected output: 'bbbbb'
