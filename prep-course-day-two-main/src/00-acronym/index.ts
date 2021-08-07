137bc8ec15cc4b52be10399437bf8e043caa046f
eamon9 <81514128+eamon9@users.noreply.github.com>
2021-08-07 13:50:38 +0300

    Update index.ts

--- prep-course-day-two-main/src/00-acronym/index.ts
+++ prep-course-day-two-main/src/00-acronym/index.ts
@@ -6,8 +6,13 @@
  * Help generate some jargon by writing a program that converts a long name like Portable Network Graphics to its acronym (PNG).
  */
 
-function parse(input: string) {
-  return "Hello!"
+function parse(input: string): string {
+  let w= input
+  .split(/ |-|_/);
+  let letters= w.map(wl => wl.charAt(0));
+  return letters
+  .join('')
+  .toUpperCase();
 }
 
 export { parse };
