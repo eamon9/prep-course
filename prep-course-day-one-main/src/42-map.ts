cf228c1cf07ec3ef0f4ad97afe574a6a2d8e136a
eamon9 <81514128+eamon9@users.noreply.github.com>
2021-08-07 11:17:05 +0300

    Update 42-map.ts

--- prep-course-day-one-main/src/42-map.ts
+++ prep-course-day-one-main/src/42-map.ts
@@ -1,5 +1,3 @@
-export {};
-
 function tidyUpString(str) {
   return str
     .trim()
@@ -12,8 +10,20 @@ function capitalise(str) {
   return str.charAt(0).toUpperCase()+ str.slice(1);
 }
 
-
 const mentors = ["/Daniel ", "irina ", " Gordon", "ashleigh "];
-let mentorsTidy= capitalise(mentors); // You are allowed to edit this line
 
-console.log(mentorsTidy); // Expected output: ["Daniel", "Irina", "Gordon", "Ashleigh"]
+// const map = (arr) => {
+//   let result = arr.map(nr => tidyUpString(nr));
+//   let result2 = result.map(nr => capitalise(nr));
+//   return result2;
+// };
+
+const mentorsTidy = function map(mentors) {
+  let result = mentors.map(nr => tidyUpString(nr));
+  let result2 = result.map(nr => capitalise(nr));
+  return result2;
+};
+
+//let mentorsTidy = map(mentors); // You are allowed to edit this line
+
+console.log(mentorsTidy(mentors)); // Expected output: ["Daniel", "Irina", "Gordon", "Ashleigh"]
