d48c36c89d4224d79c91250c3199c3e700f1dde4
eamon9 <81514128+eamon9@users.noreply.github.com>
2021-08-08 06:02:42 +0300

    Update FizzBuzz

--- prep-course-day-two-main/src/01-fizz-buzz/index.ts
+++ prep-course-day-two-main/src/01-fizz-buzz/index.ts
@@ -16,6 +16,19 @@
  * console.log('buzz')
  */
 
-function fizzBuzz(n: number) {}
+ function fizzBuzz(n: number) {
+  let number;
+  for (let i = 1; i <= n; i++) {
+    number = i;
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else 
+      console.log(number);
+  }
+}
 
 export { fizzBuzz };
